c9a33abcb3e3cc24bd70735614a121c0491ed6c1
Limbert Desir <limbert@limberts-MacBook-Pro.local>
2016-01-20 22:24:33 -0500

    final

--- helloworld.py
+++ helloworld.py
@@ -8,3 +8,22 @@
 
 # make sure that your code contains comments explaining your logic!
 
+# my first program
+
+print('Hello') # greats the user in english
+print('What is your name?') # ask the user for their name
+name = input()
+print('Choose one of the three spoken languages') # wants the user to pick a language
+print('Type \n1 Eniglish. \n2 French. \n3 Spanish.')
+language = input() # grab user input
+if language == '1': # if user input 1 program print's results
+    print('Hello')
+elif language == '2': # if user input 2 program print's results
+    print('Bonjour')
+elif language == '3':# if user input 3 program print's results
+    print('Hola')
+exit() # program exits after running 
+
+
+
+
